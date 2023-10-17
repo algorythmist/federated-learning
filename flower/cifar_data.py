6958580335862625535146adfe61de4f11a018f8
Dimitri Papaioannou <algorythmist@users.noreply.github.com>
2023-10-17 11:03:20 -0700

    more modularized flower model

---
+++ flower/cifar_data.py
@@ -0,0 +1,11 @@
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10
+from torchvision.transforms import Compose, Normalize, ToTensor
+
+
+def load_data():
+    """Load CIFAR-10 (training and test set)."""
+    trf = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    trainset = CIFAR10("./data", train=True, download=True, transform=trf)
+    testset = CIFAR10("./data", train=False, download=True, transform=trf)
+    return DataLoader(trainset, batch_size=32, shuffle=True), DataLoader(testset)
